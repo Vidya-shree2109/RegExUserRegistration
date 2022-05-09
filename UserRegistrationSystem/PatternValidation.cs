19a94214560ec4619ab624121d3701dd23fce6f3
Vidyashree N M <vidyashree2109@gmail.com>
2022-05-09 23:46:24 +0530

    [Vidya] Refactored : Pattern Validation Done For Last Name Of The User

--- UserRegistrationSystem/PatternValidation.cs
+++ UserRegistrationSystem/PatternValidation.cs
@@ -4,12 +4,21 @@ namespace UserRegistrationSystem
 {
     public class PatternValidation
     {
-        public const string FIRSTNAME_REGEX = "^[A-Za-z]{3, }$";
-        public bool ValidateFirstName(string name)
+        public const string FIRSTNAME_REGEX = "^[A-Za-z]{3,}$";
+        public const string LASTNAME_REGEX = "^[A-Za-z]{3,}$";
+        public bool ValidateFirstName(string firstName)
         {
-            if (Regex.IsMatch(name, FIRSTNAME_REGEX)) ;
+            if (Regex.IsMatch(firstName, FIRSTNAME_REGEX)) ;
             {
-                Console.WriteLine("VALID FIRST NAME ! -> " + name);
+                Console.WriteLine("VALID FIRST NAME ! -> " + firstName);
+                return true;
+            }
+        }
+        public bool ValidateLastName(string lastName)
+        {
+            if (Regex.IsMatch(lastName, LASTNAME_REGEX)) ;
+            {
+                Console.WriteLine("VALID LAST NAME ! -> " + lastName);
                 return true;
             }
         }
